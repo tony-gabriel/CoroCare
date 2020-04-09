6f554c32173f9bd03902b87d50586020d06aabdb
tony-gabriel <tonyganriel@gmail.com>
2020-04-09 16:27:51 +0100

    Tried location integration

--- app/src/main/java/com/example/coronahelpapp/MainActivity.java
+++ app/src/main/java/com/example/coronahelpapp/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.coronahelpapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
     ImageView profileImage;
     TextView profileName, status;
     ConstraintLayout profileContainer;
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+    Location userLocation;
+
+    private final long Min_Time = 10000;
+    private final long Min_Dist = 10;
+
 
 
     @Override
@@ -36,6 +54,11 @@ public class MainActivity extends AppCompatActivity {
         Movement = findViewById(R.id.button_movement);
 
         profileContainer = findViewById(R.id.profile_container);
+
+
+        MovementHistory();
+
+
 
         Test.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,6 +115,62 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, ProfileImageReg.class));
             }
         });
+
+    }
+
+    private void MovementHistory() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION},
+                    PackageManager.PERMISSION_GRANTED);
+        }
+
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        try {
+
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,Min_Time, Min_Dist, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,Min_Time, Min_Dist, locationListener);
+
+        } catch (Exception e){
+
+            e.printStackTrace();
+        }
+
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                userLocation = location;
+
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+                // TODO: Get the Latitude and Longitude and send to database
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
 
     }
 }
